ee2c22c91c93701853ff1678680e263cb20cbab1
Ahsan <Ahsan.dev.404@gmail.com>
2024-10-13 02:45:12 -0700

    feature: auth api and feature completed

--- src/api/user/user-api.ts
+++ src/api/user/user-api.ts
@@ -21,6 +21,32 @@ const userSigninRequest = async (email: string, password: string) => {
   }
 };
 
+const userSignUpRequest = async (
+  firstName: string,
+  lastName: string,
+  email: string,
+  password: string
+) => {
+  try {
+    const response = await axios.post(
+      process.env.NEXT_PUBLIC_AUTH_USER_SIGNUP as string,
+      { firstName, lastName, email, password }
+    );
+    return response;
+  } catch (error) {
+    // Check if the error is an AxiosError and type it accordingly
+    if (axios.isAxiosError(error)) {
+      // Now TypeScript recognizes that error.response is valid
+      console.error("Error Response:", error.response?.data);
+      return error.response?.data; // Access the response property safely
+    } else {
+      // Handle unexpected errors
+      console.error("Unexpected Error:", error);
+      return { message: "An unexpected error occurred" };
+    }
+  }
+};
+
 const userSigninGoogleRequest = async (tokenResponse: any) => {
   try {
     const response = await axios.post(
@@ -44,4 +70,4 @@ const userSigninGoogleRequest = async (tokenResponse: any) => {
   }
 };
 
-export { userSigninRequest, userSigninGoogleRequest };
+export { userSigninRequest, userSigninGoogleRequest, userSignUpRequest };
--- src/app/(user)/auth/signup/page.tsx
+++ src/app/(user)/auth/signup/page.tsx
@@ -1,3 +1,4 @@
+"use client";
 import React, { memo } from "react";
 import { Button } from "@/components/ui/button";
 import {
@@ -10,11 +11,60 @@ import {
 } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import { useToast } from "@/hooks/use-toast";
+import { useRouter } from "next/navigation";
 
 import { ArrowRightIcon } from "@radix-ui/react-icons";
 import Link from "next/link";
+import { userSignUpRequest } from "@/api/user/user-api";
+
+interface SignUpForm {
+  firstName: string;
+  lastName: string;
+  email: string;
+  password: string;
+}
 
 const Auth = () => {
+  const { toast } = useToast();
+  const router = useRouter();
+  const [form, setForm] = React.useState<SignUpForm>({
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: "",
+  });
+
+  const handleSignup = async (e: HTMLFormElement) => {
+    e.preventDefault();
+
+    const request = await userSignUpRequest(
+      form.firstName,
+      form.lastName,
+      form.email,
+      form.password
+    );
+
+    if (request?.error) {
+      toast({
+        variant: "destructive",
+        title: "Sign In failed",
+        description: "Email is already registered.",
+        duration: 800,
+      });
+    }
+
+    if (!request?.error && request?.data?.success) {
+      toast({
+        variant: "success",
+        title: "Sign In successful",
+        description: "Redirecting to the signin page.",
+        duration: 800,
+      });
+      router.push("/auth/signup");
+    }
+  };
+
   return (
     <div
       data-cy="main-grid"
@@ -39,23 +89,72 @@ const Auth = () => {
                 </CardDescription>
               </CardHeader>
               <CardContent>
-                <form>
+                <form
+                  onSubmit={(e: any) => {
+                    handleSignup(e);
+                  }}
+                >
                   <div className="grid w-full items-center gap-4">
                     <div className="flex flex-col space-y-1.5">
                       <Label htmlFor="fname">First Name</Label>
-                      <Input id="fname" placeholder="John" />
+                      <Input
+                        onChange={(e) =>
+                          setForm({
+                            ...form,
+                            firstName: e.target.value,
+                          })
+                        }
+                        required
+                        id="fname"
+                        name="fname"
+                        placeholder="John"
+                      />
                     </div>
                     <div className="flex flex-col space-y-1.5">
                       <Label htmlFor="lname">Last Name</Label>
-                      <Input id="lname" placeholder="Doe" />
+                      <Input
+                        onChange={(e) =>
+                          setForm({
+                            ...form,
+                            lastName: e.target.value,
+                          })
+                        }
+                        required
+                        id="lname"
+                        name="lname"
+                        placeholder="Doe"
+                      />
                     </div>
                     <div className="flex flex-col space-y-1.5">
                       <Label htmlFor="email">Email</Label>
-                      <Input id="email" placeholder="example@gmail.com" />
+                      <Input
+                        onChange={(e) =>
+                          setForm({
+                            ...form,
+                            email: e.target.value,
+                          })
+                        }
+                        id="email"
+                        name="email"
+                        type="email"
+                        required
+                        placeholder="example@gmail.com"
+                      />
                     </div>
                     <div className="flex flex-col space-y-1.5">
                       <Label htmlFor="email">Password</Label>
-                      <Input id="password" placeholder="*********" />
+                      <Input
+                        onChange={(e) =>
+                          setForm({
+                            ...form,
+                            password: e.target.value,
+                          })
+                        }
+                        id="password"
+                        name="password"
+                        placeholder="*********"
+                        required
+                      />
                     </div>
                   </div>
                   <div className="text-sm mt-4">
@@ -64,14 +163,18 @@ const Auth = () => {
                       Signin here.
                     </Link>
                   </div>
+                  <div className="flex justify-end mt-4">
+                    <Button
+                      type="submit"
+                      className=" bg-gradient-to-r from-sky-400 to-blue-500"
+                    >
+                      Continue
+                      <ArrowRightIcon className="ml-2 h-5 w-5" />
+                    </Button>
+                  </div>
                 </form>
               </CardContent>
-              <CardFooter className="flex justify-end">
-                <Button className=" bg-gradient-to-r from-sky-400 to-blue-500">
-                  Continue
-                  <ArrowRightIcon className="ml-2 h-5 w-5" />
-                </Button>
-              </CardFooter>
+              <CardFooter className="flex justify-end"></CardFooter>
             </Card>
           </div>
         </div>
